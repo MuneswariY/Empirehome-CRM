3833363605b33cac2ac4bc51ac5b44bf133b1806
C Prompt <C Prompt@DESKTOP-EBQKJSS>
2023-10-13 14:32:39 +0530

    usertype

--- Empirehomecrm/src/main/java/com/utils/Utils.java
+++ Empirehomecrm/src/main/java/com/utils/Utils.java
@@ -1,5 +1,13 @@
 package com.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class Utils {
+
+	public static void mouseHoverandClickOnElement(WebDriver driver, WebElement addnew) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
